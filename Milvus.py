89ba6325e7a4be4efd20487be9acef9229997f0d
MoLing-Dong <1970115881@qq.com>
2025-07-22 18:06:42 +0800

    更新 .env.example 文件，添加 Embedding 相关配置；修改 Milvus.py 文件，创建个人数据库及集合以存储个人信息

--- Milvus.py
+++ Milvus.py
@@ -1,5 +1,5 @@
 import os
-from pymilvus import MilvusClient
+from pymilvus import MilvusClient, CollectionSchema, FieldSchema, DataType
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -15,3 +15,19 @@ client = MilvusClient(uri=milvus_url, token=f"{milvus_user}:{milvus_password}")
 # client.create_database(db_name="my_database_23232")
 # 打印结果
 print(client.list_databases())
+
+client.create_database(db_name="personal")
+
+# 切换数据库
+client.switch_database(db_name="personal")
+
+schema = CollectionSchema(
+    fields=[
+        FieldSchema(name="id", dtype=DataType.INT64, is_primary=True, auto_id=True),
+        FieldSchema(name="embedding", dtype=DataType.FLOAT_VECTOR, dim=128),
+        FieldSchema(name="name", dtype=DataType.VARCHAR, max_length=100)
+    ],
+    description="Personal info collection"
+)
+# 创建集合
+client.create_collection(collection_name="personal_info", schema=schema)
